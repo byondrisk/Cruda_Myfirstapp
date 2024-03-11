70a149a9e28023fc1e3801dfcfd99521e8942433
Mta <matajeric2006@gmail.com>
2024-03-11 19:00:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cruda_myfirstapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.cruda_myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView myTitle;
+    TextView myName;
+    TextView myDisplay;
+    EditText myInput;
+    Button myClick;
+    Button myClear;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myTitle = findViewById(R.id.txtTitle);
+        myName = findViewById(R.id.txtName);
+        myDisplay = findViewById(R.id.txtDisplay);
+        myClick = findViewById(R.id.btnClick);
+        myClear = findViewById(R.id.btnClear);
+        myInput = findViewById(R.id.txtInputName);
+//myDisplay.setText("HELLO POGI!");
+        myClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = myInput.getText().toString();
+                myDisplay.setText("HELLO "+name+"!");
+            }
+        });
+        myClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myDisplay.setText("HELLO WORD!");
+            }
+        });
+    }
+}
+
+
